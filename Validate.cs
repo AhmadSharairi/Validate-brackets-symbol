cb6dce0124a0a896169e74221a3457887a35334a
Ahmad AlSharairi <84297675+AhmadSharairi@users.noreply.github.com>
2022-12-05 09:30:44 +0300

    Add files via upload

---
+++ Validate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Stack<char> firstStack = new Stack<char>();
+        Stack<char> secondtStack = new Stack<char>();
+
+        string check = "{(([))}";
+
+        int length = check.Length;
+        bool valid = true;
+
+
+        foreach (char c in check)
+        {
+            firstStack.Push(c);
+        }
+
+
+        while (firstStack.Count > length / 2 && length % 2 == 0)
+        {
+            secondtStack.Push(firstStack.Peek());
+            firstStack.Pop();
+        }
+
+        while (firstStack.Count > 0 && secondtStack.Count > 0)
+        {
+            if (firstStack.Peek() == '{' && secondtStack.Peek() == '}')
+            { 
+                firstStack.Pop();
+                secondtStack.Pop();
+
+            }
+            else if (firstStack.Peek() == '[' && secondtStack.Peek() == ']')
+            {
+                firstStack.Pop();
+                secondtStack.Pop();
+
+            }
+
+            else if (firstStack.Peek() == '(' && secondtStack.Peek() == ')')
+            {
+                firstStack.Pop();
+                secondtStack.Pop();
+
+            }
+
+            else
+            {
+                valid = false;
+                break;
+            }}
+
+
+        if (!valid || (valid && length % 2 != 0))
+        { Console.WriteLine("Invalid Condition!");}
+        else
+        { Console.WriteLine("Correct Is Valid ");
+           
+        }
+    }
+} 
